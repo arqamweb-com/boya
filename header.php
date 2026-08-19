e6001b7c376985bc5e533d74a367227a5e02173e
MAC <3a.sala7@gmail.com>
2026-08-19 15:07:59 +0300

    add new number for header

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ $boya_logo_alt = boya_get_site_logo_alt();
     <div class="flex items-center gap-6 opacity-95">
       <span class="flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.85 13a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.96 2.18h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
-        01017616408
+        01038304949
       </span>
       <span>توصيل سريع لكافة مناطق الجمهورية</span>
     </div>
