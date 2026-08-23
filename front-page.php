4d22049454238f3f542abdfcf359b7f2c730ecba
MAC <3a.sala7@gmail.com>
2026-08-23 13:58:11 +0300

    handle text lang

--- front-page.php
+++ front-page.php
@@ -468,8 +468,16 @@ $images = get_template_directory_uri() . '/assets/images';
           ['icon'=>'phone','title'=>__('اتصل بنا', 'arqamweb'),'value'=>"01038304949\n01038314148\n0237532216",'color'=>'var(--brand-red)'],
           ['icon'=>'message-circle','title'=>__('واتساب', 'arqamweb'),'value'=>'01038314148','color'=>'var(--brand-green)','cta'=>__('ابدأ المحادثة', 'arqamweb')],
           ['icon'=>'mail','title'=>__('البريد الإلكتروني', 'arqamweb'),'value'=>'info@boyastore-eg.com','color'=>'var(--brand-blue)'],
-          ['icon'=>'map-pin','title'=>__('العنوان', 'arqamweb'),'value'=>__('34 ش محمد حمزة البطران، متفرع من الفريق علي فهمي، المنصورية - الهرم', 'arqamweb'),'color'=>'var(--brand-orange)'],
-          ['icon'=>'clock','title'=>__('مواعيد التسليم', 'arqamweb'),'value'=>__("الطلبات من 6 صباحًا إلى 3 عصرًا تُسلّم في نفس اليوم.\nمن 4 عصرًا إلى 12 منتصف الليل تُسلّم في اليوم التالي.", 'arqamweb'),'color'=>'var(--brand-purple)'],
+                ['icon' => 'map-pin', 'title' => __('العنوان', 'arqamweb'), 'value' => __('34 ش محمد حمزة البطران، متفرع من الفريق علي فهمي، المنصورية - الهرم', 'arqamweb'), 'color' => 'var(--brand-orange)'], ['icon' => 'clock', 'title' => __('مواعيد التسليم', 'arqamweb'), 'value' => sprintf(
+                        "%s\n%s",
+                        __('الطلبات من 6 صباحًا إلى 3 عصرًا تُسلّم في نفس اليوم.', 'arqamweb'),
+                        __('من 4 عصرًا إلى 12 منتصف الليل تُسلّم في اليوم التالي.', 'arqamweb')
+                ),
+                        'color' => 'var(--brand-purple)'
+                ],
+
+
+
         ];
         $contact_icons = [
           'phone'          => '<path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.85 13a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.96 2.18h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/>',
